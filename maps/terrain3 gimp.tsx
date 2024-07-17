438660ceb7b11f54f67e0a881923328fb687aec7
Ilpo Lehtinen <feuer@feuerx.net>
2024-07-17 21:08:32 +0300

    add the existing maps into repo
    it's gonna be fun to track this binary shit with git

---
+++ maps/terrain3 gimp.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="terrain3 gimp" tilewidth="32" tileheight="32" tilecount="64" columns="32">
+ <image source="Un.png" width="1024" height="64"/>
+</tileset>
