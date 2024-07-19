844aed458fc439fd041fff6d613b01383ab6d1a9
Ilpo Lehtinen <feuer@feuerx.net>
2024-07-19 13:06:59 +0300

    fix the build script to link SDL2_Image

--- engine/build.sh
+++ engine/build.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env zsh
 
+set -euo pipefail
+
 for cpp_file in src/*.cpp
 do
     pathname=$cpp_file:t:r
     echo compiling $pathname
-    clang++ $cpp_file -g -O0 -c $(sdl2-config --cflags) -Iheaders -I/usr/local/include
+    clang++ $cpp_file -g -O0 -c $(sdl2-config --cflags) -Iheaders -I/usr/local/include -I/opt/homebrew/Cellar/sdl2_image/2.8.2_1/include/ $(pkg-config --cflags sdl2_image)
 
     # -o finropedemo  -I$headers
 done
 
-clang++ *.o -o finropedemo $(sdl2-config --libs) -lpugixml
+clang++ *.o -o finropedemo $(sdl2-config --libs) -lpugixml $(pkg-config --libs sdl2_image)
