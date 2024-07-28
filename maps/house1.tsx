cf0317fbadc7d721ba95f4838f9499928cca6fbc
Ilpo Lehtinen <feuer@feuerx.net>
2024-07-28 13:14:00 +0300

    add a smaller map

---
+++ maps/house1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="house1" tilewidth="163" tileheight="228" tilecount="1" columns="1">
+ <image source="house1.png" width="163" height="228"/>
+</tileset>
