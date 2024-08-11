51da21e25c210aeb6bfe6a1392531d57b21b37ee
Ilpo Lehtinen <feuer@feuerx.net>
2024-08-11 18:44:44 +0300

    add a `deftab` macro in resource manager to be used for defining a tab :D

--- resource_handler/src/views/root.lisp
+++ resource_handler/src/views/root.lisp
@@ -1,7 +1,7 @@
 (defpackage linnarope.views.root
   (:use :cl)
   (:import-from :linnarope.middleware :@db :*connection* :@html :@css :restored-stdout)
-  (:import-from :macro-html :html :head :link :meta :title :body :p :div)
+  (:import-from :macro-html :ul :li :html :head :link :meta :title :body :p :div :a)
   (:import-from :easy-routes :defroute)
   (:import-from :lisp-fixup :with-output-to-real-string))
 
@@ -9,22 +9,54 @@
 
 (named-readtables:in-readtable macro-html:syntax)
 
-(defroute css ("/css" :method :get :decorators (@css)) ()
-  (cl-css:css '((body
-		 :background-color "#000"
-		 :color "#fff"))))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defvar tabs (make-hash-table :test 'equal)
+    "A list of tabs, keyed by symbol and valued by their urls"))
 
-(defroute root ("/" :method :get :decorators (@html @db)) ()
-  (let ((count (getf 
-		(first
-		 (cl-dbi:fetch-all
-		  (cl-dbi:execute (cl-dbi:prepare *connection* "SELECT count(*) AS \"count-maps\" FROM map"))))
-		:|count-maps|)))
-    (html
-     (head
-      (title "Linnarope resource manager")
-      (meta :charset "utf-8")
-      (link :href "/css"  :rel "stylesheet" :type "text/css"))
-     (body
-      (div [ :class "maps"]
-	   (prin1-to-string count) " maps in the system. Add more?")))))
+(defroute css ("/css" :method :get :decorators (@css)) ()
+  (cl-css:css `((body
+		 :background-color "#fff")
+		(.topbar
+		 :display "flex"
+		 :justify-content "space-evenly"
+		 :list-style "none"
+		 :flex-direction "row")
+		(".topbar li"
+		 :border-bottom "2px solid black")
+		(".topbar li.selected"
+		 :border-bottom "5px solid black"))))
+
+(defmacro root-component (current-tab-symbol &rest contents)
+  `(html
+    (head
+     (title "Linnarope resource manager")
+     (meta :charset "utf-8")
+     (link :href "/css"  :rel "stylesheet" :type "text/css"))
+    (body
+     (ul [:class "topbar" ]
+	 (loop for k being the hash-keys of tabs
+	       for url = (gethash k tabs )
+	       collect (li [:class (if (equal ,current-tab-symbol k) "selected" "")]
+			   (a [:href url] (string-upcase k)))))
+     ,@contents)))
+
+(defmacro deftab (varlist &rest contents)
+  (destructuring-bind (tab-symbol tab-url) varlist
+    (setf (gethash tab-symbol tabs) tab-url)
+    `(defroute ,tab-symbol (,tab-url :method :get :decorators (@html @db)) ()
+       (root-component (quote ,tab-symbol) ,@contents))))
+
+(deftab (root "/")
+    (let ((count (getf 
+		  (first
+		   (cl-dbi:fetch-all
+		    (cl-dbi:execute (cl-dbi:prepare *connection* "SELECT count(*) AS \"count-maps\" FROM map"))))
+		  :|count-maps|)))
+      (div
+       (div "Hello from deftab")
+       (div [ :class "maps"]
+	    (prin1-to-string count)
+	    " maps in the system. Add more?"))))
+
+(deftab (sprites "/sprites")
+    (div "Probably no sprites yet, as the table doesn't exist"))
