5d262754d938a069568886630ea7e8b8dc99bbc1
Ilpo Lehtinen <feuer@feuerx.net>
2025-06-28 16:23:41 +0300

    WIP We're trying to implement the thing where full horizontal rows are rm'd and blocks above it are moved down.
    We're also trying to make this script actually work when loading from
    sqlite image. cl_safe_eval (or defpackage inside a progn) does
    something really stupid that C-c C-k from slime doesn't do. It seems
    like cl_safe_evalled symbols are not in the same package as symbols
    sent from slime?

--- examples/tetris-scripts/tetris.lisp
+++ examples/tetris-scripts/tetris.lisp
@@ -1,7 +1,7 @@
-(format t "Hello from tetris ~%")
-
 (defpackage examples.tetris
   (:use :cl :engine))
+
+(format t "Hello from tetris ~%")
 
 (in-package :examples.tetris)
 
@@ -47,6 +47,15 @@
     (:l-reverse 3)
     (:single 1)
     (t (error "unknown block"))))
+
+;; these might get called during re-evaluating the block class
+(defmethod height ((_ t))
+  (declare (ignore _))
+  0)
+
+(defmethod width ((_ t))
+  (declare (ignore _))
+  0)
 
 (defun flatten-once (list)
   (mapcan (lambda (i) i) list))
@@ -110,21 +119,22 @@
 		collecting (cons x y)))))))
 
 (defun render-block (bl)
-  (let ((block-sprite (get-resource "Lisp sprite" "block"))
-	(bl-x (* 50 (block-x bl)))
-	(bl-y (* 50 (block-y bl))))
-    (unless block-sprite
-      (format t "Rendering without block sprite :/~%"))
-    (when block-sprite
-      (labels ((draw (coords)
-		 (dolist (coordinate coords)
-		   (destructuring-bind (x . y) coordinate
-		     (let ((dst-x (+ bl-x (* x 50)))
-			   (dst-y (+ bl-y (* y 50))))
-		       (render block-sprite dst-x dst-y))))))
-	(draw (all-block-coords bl))))))
+  (when bl
+    (let ((block-sprite (engine:get-resource "Lisp sprite" "block"))
+	  (bl-x (* 50 (block-x bl)))
+	  (bl-y (* 50 (block-y bl))))
+      (unless block-sprite
+	(format t "Rendering without block sprite :/~%"))
+      (when block-sprite
+	(labels ((draw (coords)
+		   (dolist (coordinate coords)
+		     (destructuring-bind (x . y) coordinate
+		       (let ((dst-x (+ bl-x (* x 50)))
+			     (dst-y (+ bl-y (* y 50))))
+			 (engine:render block-sprite dst-x dst-y))))))
+	  (draw (all-block-coords bl)))))))
 
-(defparameter *last-updated* (mstimer))
+(defparameter *last-updated* (engine:mstimer))
 
 (defparameter *current-block* nil)
 
@@ -136,6 +146,8 @@
 (defparameter background-blocks (make-hash-table :test 'equal))
 
 (defun merge-and-respawn-block ()
+  (unless *current-block*
+    (format t "Wtf how is *current-block nil?~%"))
   (let* ((x (block-x *current-block*))
 	 (y (block-y *current-block*))
 	 (all-block-coordinates (all-block-coords *current-block*)))
@@ -146,9 +158,12 @@
 	  (setf (gethash (cons x y) background-blocks) (make-instance 'block :x x :y y :form :single)))))
 
     
-    (setf *current-block* (make-instance 'block :x 0 :y 0 :form (nth 
-								 (random (length *valid-blocks*))
-								 *valid-blocks*)))))
+    (setf *current-block* (make-instance 'block :x 0 :y 0 :form :square ;; (nth 
+								 ;; (random (length *valid-blocks*))
+								 ;; *valid-blocks*)
+					 ))))
+
+;; (incf (block-y *current-block*))
 
 
 (defun block-collides-with-earth ()
@@ -181,7 +196,93 @@
 	(width *current-block*))
      (1- *field-width-blocks*)))
 
+(defun draw-background ()
+  (maphash (lambda (coordinates block)
+	     (declare (ignore coordinates))
+	     (when block
+	       (render-block block)))
+	   background-blocks)
+
+  (engine:set-color 255 0 0)
+  
+  (engine:draw-line 0 0 (* *field-width-blocks* *block-w*) 0 10)
+  (engine:draw-line 0 (* *field-height-blocks* *block-w*)
+	     (* *field-width-blocks* *block-w*) (* *field-height-blocks* *block-w*)
+	     10)
+  (engine:draw-line (* *field-width-blocks* *block-w*) 0
+	     (* *field-width-blocks* *block-w*) (* *field-height-blocks* *block-w*) 10)
+  (engine:draw-line 0 0
+	     0 (* *field-height-blocks* *block-w*) 10)
+
+  (engine:set-color 0 0 0))
+
+(defun do-stupid-stuff-with-map (current-map)
+  
+  (engine:render current-map (decf map-x) (decf map-y))
+
+  (when (< map-x -900)
+    (setf map-x 900))
+
+  (when (< map-y -900)
+    (setf map-y 900)))
+
+(defun handle-kbd ()
+  (if (and (not (block-on-left-edge?))
+	   (engine:keydown? "SDLK_LEFT"))
+      (decf (block-x *current-block*))
+      )
+
+  (when (and (not (block-on-right-edge?))
+	     (engine:keydown? "SDLK_RIGHT"))
+    
+    (incf (block-x *current-block*))))
+
+(defun handle-full-rows ()
+  "Checks if there are completely filled horizontal rows that need to disappear. If there are, this function makes them disappear"
+
+  (let ((acc))
+    (dotimes (y *field-height-blocks*)
+      (let ((counter 0))
+	(dotimes (x *field-width-blocks*)
+	  (when (gethash (cons x y) background-blocks)
+	    (incf counter)))
+	(when (equalp counter *field-width-blocks*)
+	  (push y acc))))
+    ;; (when acc
+    ;;   (format t "Rows ~a seem to be full~%" acc))
+
+    (dolist (y acc)
+      (dotimes (x *field-width-blocks*)
+	(format t "Removing block at ~a, ~a~%" x y)
+	(remhash (cons x y) background-blocks))
+      
+      ;; (dotimes (yy y)
+      ;; 	(dotimes (x *field-width-blocks*)
+      ;; 	  (let ((block* (gethash (cons x yy) background-blocks)))
+      ;; 	    (remhash (cons x yy) background-blocks)
+      ;; 	    (setf (gethash (cons x (1+ yy)
+      
+      (dotimes (yy y)
+	;; we're moving blocks down bottoms-up
+	(let ((actual-y-index (- y yy)))
+	  (format t "We're moving blocks at y ~a~%" actual-y-index)
+	  (dotimes (x *field-width-blocks*)
+	    (let ((block* (gethash (cons x actual-y-index) background-blocks)))
+	      (if block*
+		  (progn
+		    (format t "Moving stuff up from ~a => ~a~%" actual-y-index (1+ actual-y-index))
+		    ;; why does this fuck everything up?
+		    (remhash (cons x actual-y-index) background-blocks)
+		    (setf (gethash (cons x (1+ actual-y-index)) background-blocks) block*))
+
+		  ;; this is expected 
+		  ;; (format t "Wtf no block at ~a~%" (cons x actual-y-index))
+		  ))))))))
+
 (defun update-game (current-map)
+
+  (handle-full-rows)
+  
   (when (or (block-collides-with-earth)
 	    (block-collides-with-other-block))
     ;; (setf finished? t)
@@ -190,52 +291,25 @@
     ;; (return-from update-game)
     )
 
-  (when (> (- (mstimer) *last-updated*) 1000)
+  (when (> (- (engine:mstimer) *last-updated*) 1000)
     (incf (block-y *current-block*))
-    (setf *last-updated* (mstimer)))
+    (setf *last-updated* (engine:mstimer)))
 
-  (when (and (not (block-on-left-edge?))
-	     (keydown? "SDLK_LEFT"))
-    (decf (block-x *current-block*)))
-
-  (when (and (not (block-on-right-edge?))
-	     (keydown? "SDLK_RIGHT"))
-    (incf (block-x *current-block*)))
+  (handle-kbd)
 
   ;; (format t "block-x ~d~%" (block-x *current-block*))
-  
-  (render current-map (decf map-x) (decf map-y))
 
-  (when (< map-x -900)
-    (setf map-x 900))
-
-  (when (< map-y -900)
-    (setf map-y 900))
+  (do-stupid-stuff-with-map current-map)
 
   (render-block *current-block*)
 
   ;; draw the background
-  (maphash (lambda (coordinates block) 
-	     (render-block block))
-	   background-blocks)
-
-  (set-color 255 0 0)
-  
-  (draw-line 0 0 (* *field-width-blocks* *block-w*) 0 10)
-  (draw-line 0 (* *field-height-blocks* *block-w*)
-	     (* *field-width-blocks* *block-w*) (* *field-height-blocks* *block-w*)
-	     10)
-  (draw-line (* *field-width-blocks* *block-w*) 0
-	     (* *field-width-blocks* *block-w*) (* *field-height-blocks* *block-w*) 10)
-  (draw-line 0 0
-	     0 (* *field-height-blocks* *block-w*) 10)
-
-  (set-color 0 0 0))
+  (draw-background))
 
 (defun update-finished ()
-  (set-color 255 0 0)
-  (draw-text "FINISHED!" 100 100)
-  (set-color 0 0 0))
+  (engine:set-color 255 0 0)
+  (engine:draw-text "FINISHED!" 100 100)
+  (engine:set-color 0 0 0))
     
 (defun update (current-map)
   (when *current-block*
@@ -246,9 +320,9 @@
 (defun setup-tetris ()
   (format t "Set up tetris! ~%"))
 
-(setf *current-block* (make-instance 'block :x 0 :y 0 :form :straight))
+(setf *current-block* (make-instance 'block :x 0 :y 0 :form :square))
 
-(setup-scene
+(engine:setup-scene
  ;; I think setup fns are completely unnecessary 
  #'setup-tetris
  #'update
