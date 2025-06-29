1ea029c5ef8e8c4d5056e040a27dcd3f66d5497a
Ilpo Lehtinen <feuer@feuerx.net>
2025-06-29 17:30:41 +0300

    experiment with gha

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if command -v brew >/dev/null 2>&1
 then
@@ -8,7 +8,6 @@ elif command -v pacman >/dev/null 2>&1
 then
     # We're probably on an Arch Linux
     pacman -S sdl2_gfx sdl2_image sdl2_ttf ecl nlohmann-json pugixml sqlite libpqxx postgresql-libs sdl3 sdl2-compat # Arch seems to not package sdl2 anymore
-    
 else
     echo "Unknown package manager/os. Currently known are brew and pacman. Pull requests welcome?"
 fi
